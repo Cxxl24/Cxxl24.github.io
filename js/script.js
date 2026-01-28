d960c71d6b90ffab1827aa833832ebe6d0ca5978
Ben van der Westhuizen <27992748@sun.ac.za>
2026-01-28 13:55:55 +0200

    Version 2

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@ window.addEventListener("scroll", () => {
 		navbar.style.backgroundColor = "#222";
 	}
 });
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+});
+
+document.querySelectorAll(".slide-in").forEach(el => {
+    observer.observe(el);
+});
